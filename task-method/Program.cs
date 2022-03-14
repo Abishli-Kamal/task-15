2d43c7a1b84248fd3557e93f5358dca4db5d792e
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-03-15 02:02:47 +0400

    task hazirdi

---
+++ task-method/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "bU tasKI metHodla yAzmAliSiz";
+            Console.WriteLine(example);
+            string task= example.Replace("bU tasKI metHodla yAzmAliSiz", "Bu Taskı Methodla Yazmalısız");
+            Console.WriteLine(task);
+        }
+    }
+}
